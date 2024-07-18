6364df68c82513e2e31d1aa5b4e85225cf7b4bbe
naitikj9 <naitikjain921@gmail.com>
2024-07-18 20:25:49 +0530

    added photo

--- client/src/routes/homePage/homePage.jsx
+++ client/src/routes/homePage/homePage.jsx
@@ -13,10 +13,7 @@ function HomePage() {
         <div className="wrapper">
           <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
-            explicabo suscipit cum eius, iure est nulla animi consequatur
-            facilis id pariatur fugit quos laudantium temporibus dolor ea
-            repellat provident impedit!
+           Find Your Dream Place With Us. We Provide The Best Service For You.
           </p>
           <SearchBar />
           <div className="boxes">
